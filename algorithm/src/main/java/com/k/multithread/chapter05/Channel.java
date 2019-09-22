abaf5212746750e0286db82fb21185745fd3fcbd
yanls <yanls@users.noreply.github.com>
2019-09-23 07:40:41 +0800

    update 20190923

---
+++ algorithm/src/main/java/com/k/multithread/chapter05/Channel.java
@@ -0,0 +1,19 @@
+package com.k.multithread.chapter05;
+
+/**
+ * 对传输通道的抽象
+ * @param <P> 产品类型
+ */
+public interface Channel<P> {
+    /**
+     * 往传输通道存入一个产品
+     */
+    void put(P product) throws InterruptedException;
+
+    /**
+     * 从传输通道中取出一个产品
+     * @return
+     * @throws InterruptedException
+     */
+    P take() throws InterruptedException;
+}
